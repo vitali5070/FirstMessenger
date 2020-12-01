40acb54fc195520ccc54d8381329658616bad047
Vitali Nabarouski <naborovski@ya.ru>
2020-12-01 14:17:21 +0300

    Initial Commit

---
+++ FirstMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstMessenger
+//
+//  Created by Vitaly Nabarouski on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
